6c059b8e7556a2c0ec5f4914b48122debf7ba86d
First Fractal <davidjesusbrunetperez@gmail.com>
2025-09-17 12:14:56 -0400

    Rebuilt mod structure; added textures loader helper; fixed jade ore implementation

---
+++ RiseoftheOldGods/RiseoftheOldGods/Content/Items/Swords/Sword.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent.UI;
+using Terraria.ID;
+
+namespace RiseoftheOldGods.Stuff.Items.Swords
+{
+    class Sword
+    {
+
+    }
+}
---
+++ RiseoftheOldGods/RiseoftheOldGods/Core/TexturesLoader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RiseoftheOldGods.Core
+{
+    public static class TexturesLoader
+    {
+        ///<summary>
+        ///    This class finds the path to a desire texture asset inside the Assets/Textures directory. It takes a string value with the name of the target folder inside 
+        ///    and the Type name value of the class that will use the target asset.
+        ///    NOTE: IT CAN ONLY WORK IF BOTH ASSETS AND CLASSES HAVE THE SAME NAME
+        ///<summary/>
+
+        private const string texturesDirctory = "RiseofTheOldGods/Assets/Textures/";   //Selfe explainatory
+
+        private static readonly Dictionary<Type, string> TexturesCache = new();   //A cache dictionary. It's particularly useful in cases when the same asset is loaded multiple times
+
+        public static string TexturesPathSolver(Type type, string targetTexturesFolder)
+        {
+            if(!TexturesCache.TryGetValue(type, out var path)) 
+            {
+                var textureName = type.Name;
+                path = $"{texturesDirctory}{targetTexturesFolder}/{textureName}";
+                TexturesCache[type] = path;
+            }
+            return path;
+        }  
+    }
+}
